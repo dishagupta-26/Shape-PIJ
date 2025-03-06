2078b48a14136306c60a88c637860c3215f45b9d
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 11:09:24 +0530

    add radius field and constructor to Circle class

--- Circle.java
+++ Circle.java
@@ -2,5 +2,11 @@ import java.lang.Math;
 
 // circle class extends shape class
 public class Circle extends Shape {
+    private double radius;
 
+    // constructor
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
 }
