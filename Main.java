29d509cbc3b5bb3540a292adbac5a02197d56a17
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 12:15:13 +0530

    add shape selection and volume calculation options in Main class

--- Main.java
+++ Main.java
@@ -20,6 +20,63 @@ public class Main {
             System.out.println("7. Exit");
 
             int choice = sc.nextInt();
+
+            Shape shape = null;
+            Volume volumeShape = null;
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the radius of the circle: ");
+                    double radius = sc.nextDouble();
+                    shape = new Circle(radius);
+                    break;
+                case 2:
+                    System.out.print("Enter the length of the rectangle: ");
+                    double length = sc.nextDouble();
+                    System.out.print("Enter the width of the rectangle: ");
+                    double width = sc.nextDouble();
+                    shape = new Rectangle(length, width);
+                    break;
+                case 3:
+                    System.out.print("Enter the side of the square: ");
+                    double side = sc.nextDouble();
+                    shape = new Square(side);
+                    break;
+                case 4:
+                    System.out.print("Enter the radius of the sphere: ");
+                    double sphereRadius = sc.nextDouble();
+                    volumeShape = new Sphere(sphereRadius);
+                    break;
+                case 5:
+                    System.out.print("Enter the radius of the cylinder: ");
+                    double cylinderRadius = sc.nextDouble();
+                    System.out.print("Enter the height of the cylinder: ");
+                    double height = sc.nextDouble();
+                    volumeShape = new Cylinder(cylinderRadius, height);
+                    break;
+                case 6:
+                    System.out.print("Enter the base side of the equilateral pyramid: ");
+                    double baseSide = sc.nextDouble();
+                    System.out.print("Enter the height of the equilateral pyramid: ");
+                    double pyramidHeight = sc.nextDouble();
+                    volumeShape = new EquilateralPyramid(baseSide, pyramidHeight);
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    continue;
+            }
+
+            if (shape != null) {
+                System.out.println("Area: " + shape.calculateArea());
+                System.out.println("Perimeter: " + shape.calculatePerimeter());
+            }
+
+            if (volumeShape != null) {
+                System.out.println("Volume: " + volumeShape.calculateVolume());
+            }
         }
     }
 }
