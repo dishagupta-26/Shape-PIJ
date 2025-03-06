d099f6f69d1cb9540826798ac606c620b7e9c78f
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 11:43:48 +0530

    add constructor to Cylinder class to initialize radius and height fields

--- Cylinder.java
+++ Cylinder.java
@@ -1,4 +1,11 @@
 // cylinder class estends shape class implements volume interface
 public class Cylinder {
     private double radius, height;
+
+    // constructor
+    public Cylinder(double radius, double height) {
+        super("Cylinder");
+        this.radius = radius;
+        this.height = height;
+    }
 }
