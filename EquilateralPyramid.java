89ec50289a84c224655aca2975dd3f082152124b
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 11:52:58 +0530

    add constructor to EquilateralPyramid class to initialize baseSide and height fields

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -2,4 +2,11 @@
 public class EquilateralPyramid extends Shape implements Volume {
     private double baseSide, height;
 
+    // constructor
+    public EquilateralPyramid(double baseSide, double height) {
+        super("Equilateral Pyramid");
+        this.baseSide = baseSide;
+        this.height = height;
+    }
+
 }
