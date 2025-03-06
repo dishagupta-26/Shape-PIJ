0129b18d76986d01b747e39e5305a297f41a31fc
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 12:04:39 +0530

    add menu for shape selection in Main class

--- Main.java
+++ Main.java
@@ -8,5 +8,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // creating the menu
+        while (true) {
+            System.out.println("\nChoose a shape:");
+            System.out.println("1. Circle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Square");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Equilateral Pyramid");
+            System.out.println("7. Exit");
+
+            int choice = sc.nextInt();
+        }
     }
 }
