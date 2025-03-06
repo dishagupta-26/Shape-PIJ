e229d981dacf8318509235e9c362bd8b25c437ae
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 12:02:09 +0530

    add Main class with main method and Scanner initialization

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Name: Disha Deepak Gupta
+// PRN: 24070126513
+// Batch: A2
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
