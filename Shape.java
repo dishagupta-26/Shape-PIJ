20afd0ca149a13cb19b5fd688bd31b332b19bbb4
Disha Gupta <gupta.disha.d26@gmail.com>
2025-03-06 11:03:44 +0530

    add abstract methods for calculating area and perimeter in Shape class

--- Shape.java
+++ Shape.java
@@ -6,4 +6,9 @@ public abstract class Shape {
     public Shape(String shapeName) {
         this.shapeName = shapeName;
     }
+
+    // abstract method for calculating area and perimeter
+    public abstract double calculateArea();
+
+    public abstract double calculatePerimeter();
 }
